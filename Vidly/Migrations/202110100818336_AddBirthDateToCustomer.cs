111cdbc37813bf2af3ad887fa4d84cbdd5885ecc
Raza Saleemi <rks143@gmail.com>
2021-10-10 13:29:52 +0500

    Section 3 Exercise 2: Add BirthDate to Customers and show if it has value.

---
+++ Vidly/Migrations/202110100818336_AddBirthDateToCustomer.cs
@@ -0,0 +1,19 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddBirthDateToCustomer : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Customers", "BirthDate", c => c.DateTime());
+            Sql("UPDATE Customers SET  BirthDate='1/1/1980' WHERE Id = 1");
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Customers", "BirthDate");
+        }
+    }
+}
