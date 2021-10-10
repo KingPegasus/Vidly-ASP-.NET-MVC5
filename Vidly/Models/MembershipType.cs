773e909efde5635aff1cfb847b8f558e5f0388d1
Raza Saleemi <rks143@gmail.com>
2021-10-10 23:32:52 +0500

    Custom Validation and Refactoring Magic Numbers.

--- Vidly/Models/MembershipType.cs
+++ Vidly/Models/MembershipType.cs
@@ -7,5 +7,13 @@
         public short SignUpFee { get; set; }
         public byte DurationInMonths { get; set; }
         public byte DiscountRate { get; set; }
+
+        public static readonly byte Unknown = 0;
+        public static readonly byte Mem0 = 1;
+        public static readonly byte Mem10 = 2;
+        public static readonly byte Mem15 = 3;
+        public static readonly byte Mem20 = 4;
+
+
     }
 }
