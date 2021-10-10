1ec2850f35ccef73049ec49a268d69897ba6247f
Raza Saleemi <rks143@gmail.com>
2021-10-10 13:08:10 +0500

    Section 3 Exercise 1: Adding Name to Membership Type and show Membership Type against each customer.

---
+++ Vidly/Migrations/202110100753530_AddNameToMembershipType.cs
@@ -0,0 +1,21 @@
+﻿namespace Vidly.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    public partial class AddNameToMembershipType : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.MembershipTypes", "Name", c => c.String());
+            Sql("UPDATE MembershipTypes SET  Name='Mem0' WHERE Id = 1");
+            Sql("UPDATE MembershipTypes SET  Name='Mem10' WHERE Id = 2");
+            Sql("UPDATE MembershipTypes SET  Name='Mem15' WHERE Id = 3");
+            Sql("UPDATE MembershipTypes SET  Name='Mem20' WHERE Id = 4");
+        }
+
+        public override void Down()
+        {
+            DropColumn("dbo.MembershipTypes", "Name");
+        }
+    }
+}
