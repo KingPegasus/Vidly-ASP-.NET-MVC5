a86613f4f30a10495bc3d7be8bc756ba4535a5b3
Raza Saleemi <rks143@gmail.com>
2021-10-13 23:10:06 +0500

    Seeding Users and Roles.

--- Vidly/Controllers/AccountController.cs
+++ Vidly/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using System.Linq;
@@ -152,6 +153,13 @@ namespace Vidly.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+
+                    // Temmp code
+                    //var roleStore = new RoleStore<IdentityRole>(new ApplicationDbContext());
+                    //var roleManager = new RoleManager<IdentityRole>(roleStore);
+                    //await roleManager.CreateAsync(new IdentityRole("CanManageMovies"));
+                    //await UserManager.AddToRolesAsync(user.Id, "CanManageMovies");
+
                     await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
 
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
---
+++ Vidly/Migrations/202110131716264_SeedUsers.cs
@@ -0,0 +1,25 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SeedUsers : DbMigration
+    {
+        public override void Up()
+        {
+            Sql(@"
+INSERT INTO [dbo].[AspNetUsers] ([Id], [Email], [EmailConfirmed], [PasswordHash], [SecurityStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEndDateUtc], [LockoutEnabled], [AccessFailedCount], [UserName]) VALUES (N'0a125a33-beee-41b0-b2e3-632b0a326c86', N'guest@vidly.com', 0, N'AKnZ+45h3Lr8v11lujVnHA2zMEg976f7nnkP6PDCMypZe8OU91sbRN+y6kd5Cs4tdg==', N'9a90eec6-6204-4a50-8df2-079ed8cea4a5', NULL, 0, 0, NULL, 1, 0, N'guest@vidly.com')
+INSERT INTO [dbo].[AspNetUsers] ([Id], [Email], [EmailConfirmed], [PasswordHash], [SecurityStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEndDateUtc], [LockoutEnabled], [AccessFailedCount], [UserName]) VALUES (N'70a22c51-ce00-4272-8b25-a76829eb00ba', N'admin@vidly.com', 0, N'ANnJ2ce6HeSWKXN7/BHfheTe0+s1XpAZR0nxyPRVFDHI2dcr9woR6CC4iecdxv+bbA==', N'34cba9eb-ba1f-41c4-9ad6-7d1ad4ab364d', NULL, 0, 0, NULL, 1, 0, N'admin@vidly.com')
+
+INSERT INTO [dbo].[AspNetRoles] ([Id], [Name]) VALUES (N'c0504fb5-4ba5-4275-9a7d-a9b98a75edfb', N'CanManageMovies')
+
+INSERT INTO [dbo].[AspNetUserRoles] ([UserId], [RoleId]) VALUES (N'70a22c51-ce00-4272-8b25-a76829eb00ba', N'c0504fb5-4ba5-4275-9a7d-a9b98a75edfb')
+
+            ");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
